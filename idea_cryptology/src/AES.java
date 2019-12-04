b2ed6b19cf1cec42511e4dd3949c618d864434fd
ColaLinN <782907833@qq.com>
2019-12-04 17:14:34 +0800

    "idea test"

---
+++ idea_cryptology/src/AES.java
@@ -0,0 +1,29 @@
+public class AES {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+    public void Rijndael(){
+
+    }
+    public void KeyExpansion(){
+
+    }
+    public void AddRoundKey(){
+
+    }
+    public void Round(){
+
+    }
+    public void FinalRound(){
+
+    }
+    public void ByteSub(){
+
+    }
+    public void ShiftRow(){
+
+    }
+    public void MixColumn(){
+
+    }
+}
